168996e0e52145b25d4f432a10042e98daddb861
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 12:13:54 +0200

    dropDownBlogsMenu

--- JS/navBar.js
+++ JS/navBar.js
@@ -2,6 +2,13 @@ function createNavBar() {
     document.getElementsByTagName("nav")[0].innerHTML +=
         `<a href="/WSOA3028A_1827593/index.html">Home Page</a>
         <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">My Blogs</a> 
+            <a class="dropbtn">Dropdown
+                <i class="fa fa-caret-down"></i>
+            </a>
+            <div class="dropdown-content">
+                 <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">Blog 1</a>
+                 <a href="/WSOA3028A_1827593/BlogPosts/BlogPost2.html">Blog 2</a>
+            </div>
         <a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a> 
         <a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a>`;
 }
