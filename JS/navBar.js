e5435cc12945c2987a9455e4ad2d595596e0c0b9
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 12:07:18 +0200

    addNavBarToEachPage

--- JS/navBar.js
+++ JS/navBar.js
@@ -1,6 +1,7 @@
 function createNavBar() {
     document.getElementsByTagName("nav")[0].innerHTML +=
-        `<a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">My Blogs</a> 
+        `<a href="/WSOA3028A_1827593/index.html">Home Page</a>
+        <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">My Blogs</a> 
         <a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a> 
         <a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a>`;
 }
