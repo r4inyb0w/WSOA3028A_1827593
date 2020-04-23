84d9afe5b16dd1168c408c9328251eba98af4ce3
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 14:12:07 +0200

    updateNavBar

--- JS/navBar.js
+++ JS/navBar.js
@@ -1,7 +1,16 @@
 function createNavBar() {
     document.getElementsByTagName("nav")[0].innerHTML +=
-        `<a href="/WSOA3028A_1827593/index.html">Home Page</a>
-        <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">My Blogs</a> 
-        <a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a> 
-        <a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a>`;
+        `<ul>
+            <li><a href="/WSOA3028A_1827593/index.html">Home Page</a></li>
+            <li><a href="#">My Blogs</a>
+              <ul>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">Blog 1</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/BlogPost2.html">Blog 2</a></li>
+                </ul>
+            </li>
+            <li><a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a></li>
+            <li><a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a></li>
+        </ul>`;
 }
+
+
