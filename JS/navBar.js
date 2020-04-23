5f535a9a4398bfa833115b144e6981cddedf4184
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 12:24:34 +0200

    removeExample

--- JS/navBar.js
+++ JS/navBar.js
@@ -2,15 +2,6 @@ function createNavBar() {
     document.getElementsByTagName("nav")[0].innerHTML +=
         `<a href="/WSOA3028A_1827593/index.html">Home Page</a>
         <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">My Blogs</a> 
-        <div class ="dropdown">
-            <button class="dropbtn">Dropdown
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-                 <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">Blog 1</a>
-                 <a href="/WSOA3028A_1827593/BlogPosts/BlogPost2.html">Blog 2</a>
-            </div>
-        </div>
         <a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a> 
         <a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a>`;
 }
