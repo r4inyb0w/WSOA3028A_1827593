149c37017774750327cacea162239ea792743289
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 14:43:04 +0200

    dropDownStyling

--- JS/navBar.js
+++ JS/navBar.js
@@ -1,16 +1,16 @@
 function createNavBar() {
     document.getElementsByTagName("nav")[0].innerHTML +=
-        `
-            <a href="/WSOA3028A_1827593/index.html">Home Page</a></li>
-            <a href="#">My Blogs
+        `<ul>
+            <li><a href="/WSOA3028A_1827593/index.html">Home Page</a></li>
+            <li><a href="#">My Blogs</a>
               <ul>
-                 <a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">Blog 1</a>
-                 <a href="/WSOA3028A_1827593/BlogPosts/BlogPost2.html">Blog 2</a>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">Blog 1</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/BlogPost2.html">Blog 2</a></li>
                 </ul>
-            </a>
-            <a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a>
-            <a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a>
-        `;
+            </li>
+            <li><a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a></li>
+            <li><a href="/WSOA3028A_1827593/PoliticsPage/MyPolitics.html">My Politics</a></li>
+        </ul>`;
 }
 
 
