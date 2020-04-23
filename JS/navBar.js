547a5e805de44d5c69ef1e5f920192c050d45961
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 11:52:45 +0200

    addingJSnavbar

---
+++ JS/navBar.js
@@ -0,0 +1,4 @@
+function createNavBar() {
+    document.getElementsByTagName("nav")[0].innerHTML +=
+        '<a href="../BlogPosts/BlogPost1.html">My Blogs</a> <a href="../FlowersPage/MyFlowers.html">My Flowers</a> <a href="../PoliticsPage/MyPolitics.html">My Politics</a>';
+}
