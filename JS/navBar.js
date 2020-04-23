3e714badad6165d2eb691a4c1fce538a00245e6b
r4inyb0w <1827593@students.wits.ac.za>
2020-04-23 21:12:21 +0200

    updateDropdown

--- JS/navBar.js
+++ JS/navBar.js
@@ -4,8 +4,19 @@ function createNavBar() {
             <li><a href="/WSOA3028A_1827593/index.html">Home Page</a></li>
             <li><a href="#">My Blogs</a>
               <ul>
-                 <li><a href="/WSOA3028A_1827593/BlogPosts/BlogPost1.html">Blog 1</a></li>
-                 <li><a href="/WSOA3028A_1827593/BlogPosts/BlogPost2.html">Blog 2</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/As-we-may-think-1945.html">Blog 1</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Library-of-Babel-and-The-Garden-of-Forking-Paths.html">Blog 2</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Character-Description.html">Blog 3</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Wireframes-and-Style-Guide.html">Blog 4</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Theodore-H-Nelson-1981.html">Blog 5</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Why-the-dot-com-bubble-was-an-important-part-of-internet-history.html">Blog 6</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/The-link-between-changing-technology-and-the-development-and-spread-of-internet-culture.html">Blog 7</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Semantic-Mark-up-and-HTML5.html">Blog 8</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Metadata.html">Blog 9</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Events-and-Interactivity.html">Blog 10</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Online-learning-and-teaching.html">Blog 11</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/Character-exploration-Flowers-and-their-meanings.html">Blog 12</a></li>
+                 <li><a href="/WSOA3028A_1827593/BlogPosts/How-would-Macey-Daniels-deal-with-the-COVID-19-pandemic.html">Blog 13</a></li>
                 </ul>
             </li>
             <li><a href="/WSOA3028A_1827593/FlowersPage/MyFlowers.html">My Flowers</a></li>
