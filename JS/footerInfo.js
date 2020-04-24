636472b7dc24add873aa8c0665c2c57bc7fbd41d
r4inyb0w <1827593@students.wits.ac.za>
2020-04-24 11:44:47 +0200

    updatefooter

---
+++ JS/footerInfo.js
@@ -0,0 +1,11 @@
+function createFooterInfo() {
+    document.getElementsByTagName("footer")[0].innerHTML +=
+        ` <section class="h-card">
+        <p style="display: inline; padding-right: 20px;" class="p-tel">Contact Number: 011 450 8221 </p>
+        <p style="display: inline; padding-right: 20px;" class="p-post-office-box"> P.O. Box 1212, Highway Gardens,
+            Edenvale </p>
+        <p style="display: inline;" class="p-street-address"> Daniels' Daisies Address: 98 1st Rd, Dunvegan,
+            Johannesburg, 2090</p>
+        <p>Latest Update: 8 November 1990</p>
+    </section>`;
+}
