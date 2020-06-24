1f3af8fe18312489f0e86fb54f8298c57d751940
r4inyb0w <1827593@students.wits.ac.za>
2020-06-24 19:08:57 +0200

    added twitter link

--- JS/footerInfo.js
+++ JS/footerInfo.js
@@ -5,8 +5,13 @@ function createFooterInfo() {
         <p style="display: inline; padding-right: 20px;" class="p-tel">Contact Number: 011 450 8221 </p>
         <p style="display: inline; padding-right: 20px;" class="p-post-office-box"> P.O. Box 1212, Highway Gardens,
             Edenvale </p>
-        <p style="display: inline;" class="p-street-address"> Daniels' Daisies Address: 98 1st Rd, Dunvegan,
+        <p style="display: inline; padding-right: 20px;" class="p-street-address"> Daniels' Daisies Address: 98 1st Rd, Dunvegan,
             Johannesburg, 2090</p>
+        <p style="display: inline; ">
+            <a href="https://mobile.twitter.com/FlowerProtest89"><img class="twitter" width= 1%;
+            src="../WSOA3028A_1827593/Images/twitterLogo.png"
+            alt="Twitter logo"></a>
+        </p>
         <p>Latest Update: 20 March 1990</p>
     </section>`;
 }
