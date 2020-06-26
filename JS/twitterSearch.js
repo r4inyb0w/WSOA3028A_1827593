c2bfcfe5c04bba76d22313ddc68e4bfe648e0c11
r4inyb0w <1827593@students.wits.ac.za>
2020-06-26 21:46:23 +0200

    added BLM content

---
+++ JS/twitterSearch.js
@@ -0,0 +1,15 @@
+/*const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://api.twitter.com/1.1/search/tweets.json?q=%23PurpleFlowerProtest89&result_type=popular";
+var myHeaders = new Headers();
+
+myHeaders.append('Content-Type', 'application/json');
+myHeaders.get('Content-Type');
+
+fetch(url, { headers: 'Access-Control-Allow-Origin' + myHeaders })
+    .then((r) => r.json())
+    .then((tweet) => {
+        console.log(tweet.text);
+    })
+    .catch((error) => console.warn("My Warning", error));
+
+console.log("yo!");*/
